5b583dafdd7d249d6b2180b69e66595978581b59
Kyungmin Han <kyungmin.han@wesang.com>
2022-12-12 16:52:18 +0900

    SPM 대응 코드 수정.

--- Package.swift
+++ Package.swift
@@ -5,16 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "Moya-ObjectMapper",
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
         .library(name: "Moya-ObjectMapper", targets: ["Moya-ObjectMapper"]),
         .library(name: "Moya-RxSwift-ObjectMapper", targets: ["Moya-RxSwift-ObjectMapper"]),
         .library(name: "Moya-ReactiveSwift-ObjectMapper", targets: ["Moya-ReactiveSwift-ObjectMapper"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "12.0.0")),
-        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "3.4.0")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "4.5.0")),
-        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", .upToNextMajor(from: "4.0.0"))
+        .package(url: "https://github.com/Moya/Moya.git", from: .init(14, 0, 0)),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", from: .init(4, 2, 0)),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: .init(5, 1, 1)),
+        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: .init(6, 1, 0))
     ],
     targets: [
         .target(
@@ -29,9 +32,11 @@ let package = Package(
             name: "Moya-RxSwift-ObjectMapper",
             dependencies: [
                 "Moya",
-                "RxMoya",
+                "Moya-ObjectMapper",
                 "ObjectMapper",
-                "RxSwift"],
+                "RxSwift",
+                .product(name: "RxMoya", package: "Moya")
+            ],
             path: "Source/RxSwift",
             exclude: [
                 "Sample"]),
@@ -39,9 +44,11 @@ let package = Package(
             name: "Moya-ReactiveSwift-ObjectMapper",
             dependencies: [
                 "Moya",
-                "ReactiveMoya",
                 "ObjectMapper",
-                "ReactiveSwift"],
+                "Moya-ObjectMapper",
+                "ReactiveSwift",
+                .product(name: "ReactiveMoya", package: "Moya")
+            ],
             path: "Source/ReactiveSwift",
             exclude: [
                 "Sample"])
